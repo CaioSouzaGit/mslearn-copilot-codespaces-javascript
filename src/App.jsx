e06a2825959f3c49536a4151e4c28f059164b18f
Caio Souza <souzacaio20@gmail.com>
2025-09-06 15:51:12 +0000

    Exercita github copilot

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Caio Souza",
+  title: "Engenheiro de Software SR",
+  email: "caio.souza@sinqia.com.br",
+  gitHub: "https://github.com/CaioSouzaGit",
+  instagram: "https://www.instagram.com/lecaiosouza",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/CaioFlawn",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
